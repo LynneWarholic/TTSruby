8bc0bfd2bdf188637580140c901038cf3be93403
23kpm <lynnewarholic@gmail.com>
2015-04-30 00:06:41 +0000

    madlibs

---
+++ height_to_cent.rb
@@ -0,0 +1,5 @@
+puts "Hi! What is your height in inches?"
+height_in = gets.chomp
+height_cm = 2.54 * height_in.
+puts "Great! Your height in centimeters is " + height_cm + "."
+*/This doesn't work
---
+++ madlib.rb
@@ -0,0 +1,26 @@
+puts "Hello! What's your name?"
+name = gets.chomp
+
+puts "Tell me the name of your favorite person."
+person = gets.chomp
+
+puts "Tell me your favorite animal."
+animal = gets.chomp
+
+puts "Tell me a body part."
+body = gets.chomp
+
+puts "Tell me a color."
+color = gets.chomp
+
+puts "Tell me your favorite food."
+food = gets.chomp
+
+puts "Thanks " + name + "! Here's your own Mary had a little lamb..."
+puts ""
+puts person + " had a little " + animal + ","
+puts "it's " + body + " was " + color + " as " + food + "."
+puts "and everywhere that " + person + " went "
+puts "the " + animal + " was sure to go!"
+puts ""
+puts "That was great, wasn't it?"
